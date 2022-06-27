fc2f603920c509cdf9e08b4a3c691f2a5454ce0f
Magsut <maqsutyangiboyev@gmail.com>
2022-06-27 20:28:53 +0500

    pom

--- src/main/java/com/example/lasttask/config/SecurityConfig.java
+++ src/main/java/com/example/lasttask/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -56,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(new ArrayList<>(Arrays.asList("*")));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers",
                 "Access-Control-Allow-Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers",
