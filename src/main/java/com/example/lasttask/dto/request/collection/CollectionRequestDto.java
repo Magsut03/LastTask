4225aea8c7f312ac8d620dd0679f964437f36694
Magsut <maqsutyangiboyev@gmail.com>
2022-07-03 23:59:58 +0500

    ishla

--- src/main/java/com/example/lasttask/dto/request/collection/CollectionRequestDto.java
+++ src/main/java/com/example/lasttask/dto/request/collection/CollectionRequestDto.java
@@ -14,5 +14,5 @@ public class CollectionRequestDto {
     private String topic;
     private String name;
     private String description;
-    private MultipartFile imageFile;
+    private String imageFile;
 }
